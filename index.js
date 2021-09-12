ca150a5308895700978e5c9df72410b05543c473
gurelbs <guri240@gmail.com>
2021-09-12 23:32:40 +0300

    updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ express()
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
   .get('/', (req, res) => {
-    res.sendFile(__dirname + 'build/index.html')
+    res.sendFile(__dirname + 'client/build/index.html')
   })
   .use('/api', require('./api'))
   .listen(process.env.PORT||4000, () => console.log(`server run at http://localhost:4000`))
